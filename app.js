7912ef53ddc523513f9b516e60d8a4c46c19a404
Mary8992 <mc046569@gmail.com>
2023-05-04 14:56:43 +0300

    js codes and 3D functionality added

---
+++ app.js
@@ -0,0 +1,13 @@
+VanillaTilt.init(document.querySelectorAll(".box"), {
+    max: 25,
+    speed: 400,
+});
+
+/*
+VanillaTilt is the name of the library.
+.init is a method in the VanillaTilt library that initializes the tilt effect on the specified elements.
+document.querySelectorAll(".box") selects all the elements with the class name "box" on the web page and passes them as a parameter to the init method. This tells the library to apply the tilt effect to these elements.
+max: 25 sets the maximum tilt rotation in degrees. In this case, it's set to 25 degrees.
+speed: 400 sets the speed of the tilt animation. In this case, it's set to 400 milliseconds.
+Overall, this code is designed to apply the 3D tilt effect to all elements with the class name "box" using the VanillaTilt library. The max and speed options are set to customize the tilt effect for the specified elements.
+ */
